4becec54887e5f7b390a4c6915757ed2f8252b4c
berendeev <sellect.47@gmail.com>
2017-12-18 23:26:33 +0600

    add comments

--- lab2GDvar6/src/Main.java
+++ lab2GDvar6/src/Main.java
@@ -1,54 +1,54 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {		//точка входа
+		Scanner scanner = new Scanner(System.in);	//Для чтения с коноли
 		System.out.println("a1 = a + bi\na= ");
 		double a = scanner.nextDouble();
 		System.out.println("b= ");
 		double b = scanner.nextDouble();
-		ComplexNumber a1 = new ComplexNumber(a, b);
+		ComplexNumber a1 = new ComplexNumber(a, b);	//создаем обьект класса
 
 		System.out.println("a2 = a + bi\na= ");
 		a = scanner.nextDouble();
 		System.out.println("b= ");
 		b = scanner.nextDouble();
-		ComplexNumber a2 = new ComplexNumber(a, b);
+		ComplexNumber a2 = new ComplexNumber(a, b);	//2 обьект
 
 		System.out.println("a3 = a + bi\na= ");
 		a = scanner.nextDouble();
 		System.out.println("b= ");
 		b = scanner.nextDouble();
-		ComplexNumber a3 = new ComplexNumber(a, b);
+		ComplexNumber a3 = new ComplexNumber(a, b);	//3 обьект
 
 		ComplexNumber x = a2.pow(4).add((a1.substruct(a2).division((a3.multiplication(a1)))));//формула из задания
-		x.out();
+		x.out();	//вывод комплексного числа
 
 		System.out.println(x.abs());
 	}
 }
 
 class ComplexNumber {
-	private double integer;
-	private double imaginary;
+	private double integer; //целая часть
+	private double imaginary;	// мнимая часть
 
-	ComplexNumber(double integer, double imaginary) {
+	ComplexNumber(double integer, double imaginary) {	//конструктор с 2 аргументами
 		this.integer = integer;
 		this.imaginary = imaginary;
 	}
 
 	public double abs() {
 		return Math.sqrt(integer * integer + imaginary * imaginary);
-	}
+	}	//модуль
 
-	public ComplexNumber add(ComplexNumber complexNumber) {
+	public ComplexNumber add(ComplexNumber complexNumber) {	//сложение
 		return new ComplexNumber(integer + complexNumber.integer, imaginary + complexNumber.imaginary);
 	}
 
-	public ComplexNumber substruct(ComplexNumber complexNumber) {
+	public ComplexNumber substruct(ComplexNumber complexNumber) { //вычитание
 		return new ComplexNumber(integer - complexNumber.integer, imaginary - complexNumber.imaginary);
 	}
-
+	//умножение
 	public ComplexNumber multiplication(ComplexNumber complexNumber) {//(a+bi)(c+di)= ac+adi+cbi+bidi=AC+ADi+CBi+(-BD);
 		double intPart; // = AC - BD
 		double imPart;  // = AD + CB
@@ -58,7 +58,7 @@ class ComplexNumber {
 
 		return new ComplexNumber(intPart, imPart);
 	}
-
+	//деление
 	public ComplexNumber division(ComplexNumber complexNumber) {
 		ComplexNumber conjugate = new ComplexNumber(complexNumber.integer, -complexNumber.imaginary);//сопряженное
 		ComplexNumber numerator = conjugate.multiplication(this);//перемножаем чисдитель и сопряженное this - этот класс
@@ -74,7 +74,6 @@ class ComplexNumber {
 		ComplexNumber temp = new ComplexNumber(integer, imaginary);
 		for (int i = 0; i < power - 1; i++) {
 			temp = temp.multiplication(temp);
-			temp.out();
 		}
 		return temp;
 	}
